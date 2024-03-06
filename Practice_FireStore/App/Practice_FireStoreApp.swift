4161dd1d93c3a79eedbea9aa80856aa88e2acf3d
Jiho <yjh7827@naver.com>
2024-03-06 15:24:07 +0900

    Feat: initial commit

---
+++ Practice_FireStore/App/Practice_FireStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_FireStoreApp.swift
+//  Practice_FireStore
+//
+//  Created by 유지호 on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Practice_FireStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Practice_FireStore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Practice_FireStore
+//
+//  Created by 유지호 on 3/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
